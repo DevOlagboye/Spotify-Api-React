a5aec92edcf78929af5646206a4bad518c4355b9
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-13 16:24:46 +0100

    Installed dotenv dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <p>Artist Name:</p>
+        <p>Artist Followers:</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
