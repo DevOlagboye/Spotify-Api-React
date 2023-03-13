88dcdbd4ec11b69dc3bc9dae36f4ef2f6c719c04
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-13 17:21:05 +0100

    Worked on fetching API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [res, setRes] = useState([]);
+  const fetchRequest = async () => {
+    const data = await fetch(
+      `https://api.spotify.com/v1/artists/${process.env.REACT_APP_ARTIST_ID}`
+    );
+    const dataJ = await data.json();
+    const result = dataJ.results;
+    console.log(result);
+    setRes(result);
+  };
+  useEffect(() => {
+    fetchRequest();
+  }, []);
   return (
     <div className="App">
       <div>
