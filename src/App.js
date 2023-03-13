ecb829f6fc1c56de954ecaddd679dc40463e38b1
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-13 18:21:34 +0100

    Want to install axios to make HTTP requests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
 
 function App() {
   const [res, setRes] = useState([]);
@@ -37,6 +38,19 @@ function App() {
     setToken("");
     window.localStorage.removeItem("token");
   };
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params : {
+        q : searchKey,
+        type: "artist"
+      }
+    })
+    setArtists(data.artists.items)
+  }
   return (
     <div className="App">
       <div>
@@ -50,6 +64,10 @@ function App() {
         ) : (
           <button onClick={logout}>Logout</button>
         )}
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+          <button type={"submit"}>Search</button>
+        </form>
         <p>Artist Name:</p>
         <p>Artist Followers:</p>
       </div>
