d16791c786e74a71856868eda42efc39863cf10d
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-13 17:59:51 +0100

    Finished the connection and setting the APIs will soon start fetching the APIs

--- src/App.js
+++ src/App.js
@@ -3,21 +3,51 @@ import "./App.css";
 
 function App() {
   const [res, setRes] = useState([]);
-  const fetchRequest = async () => {
-    const data = await fetch(
-      `https://api.spotify.com/v1/artists/${process.env.REACT_APP_ARTIST_ID}`
-    );
-    const dataJ = await data.json();
-    const result = dataJ.results;
-    console.log(result);
-    setRes(result);
-  };
+  const [token, setToken] = useState("");
+  // const fetchRequest = async () => {
+  //   const data = await fetch(
+  //     `https://api.spotify.com/v1/artists/${process.env.REACT_APP_ARTIST_ID}`
+  //   );
+  //   const dataJ = await data.json();
+  //   const result = dataJ.results;
+  //   console.log(result);
+  //   setRes(result);
+  // };
   useEffect(() => {
-    fetchRequest();
+    // fetchRequest();
+    const hash = window.location.hash;
+    const token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.localStorage.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+    setToken(token);
   }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
   return (
     <div className="App">
       <div>
+        <h1>Spotify React</h1>
+        {!token ? (
+          <a
+            href={`${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}`}
+          >
+            Login
+          </a>
+        ) : (
+          <button onClick={logout}>Logout</button>
+        )}
         <p>Artist Name:</p>
         <p>Artist Followers:</p>
       </div>
